1ae105061aa9ce8411931edba2c6c959726ccc9b
maksim <maksim@Mac-mini-maksim.local>
2021-12-10 12:49:30 +0300

    ffix: sending file to client side

--- server/compress.js
+++ server/compress.js
@@ -3,7 +3,7 @@ const { resolve } = require('path');
 
 function compressModel(params) {
   return new Promise((res, rej) => {
-    exec(`gltfpack ${params.compression} -i ${params.inputFile} -o ${params.outputFile}`,
+    exec(`gltfpack -i ${params.inputFile} -o ${params.outputFile} ${params.compression}`,
       { shell: true }, (err, stdout, stderr) => {
         if (err) rej(err);
         if (stdout) rej(stdout);
--- server/server.js
+++ server/server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const cors = require('cors');
-const multer = require('multer')
+const multer = require('multer');
 const fs = require('fs');
+const path = require('path');
 const { promises } = require('fs');
 const compressModel = require('./compress');
 const createGLBFile = require('./createGLBFile');
@@ -19,17 +20,26 @@ app.post('/compress', upload.single('model'), async (req, res) => {
 
   const inputFilePath = 'server/models/model.glb'
   const outputFilePath = 'server/models/compressedModel.glb'
+
   try {
     const filePath = req.file.path;
 
     if (fs.existsSync(filePath)) {
 
-      const data = await promises.readFile(filePath)
-      await createGLBFile(data)
-      await compressModel(({ compression: '-c', inputFile: inputFilePath, outputFile: outputFilePath }));
+      const data = await promises.readFile(filePath);
+      await createGLBFile(data);
+      await compressModel(({ compression: '-si 0.1', inputFile: inputFilePath, outputFile: outputFilePath }));
 
-      const model = await promises.readFile(outputFilePath);
-      return res.json({ model: model });
+      // const model = await promises.readFile(inputFilePath);
+      // const model1 = await promises.readFile(outputFilePath)
+
+        return res.sendFile('/models/compressedModel.glb', { root: path.join(__dirname)}, function (err) {
+        if (err) {
+          console.error(err);
+        } else {
+          console.log('Sent:', outputFilePath);
+        }
+      });
     }
   } catch (e) {
     console.error(e);
